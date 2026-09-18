85f9603f1a1750bc5125d175b7f23a547273546f
Eloá Machado Martines <eloamachadomartines1@gmail.com>
2026-09-18 16:54:19 -0400

    Initialize Django project structure with basic settings, URLs, and user app

---
+++ usuarios/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class Usuario(models.Model):
+# Isso é a forma do Django de criar um campo com opções fixas
+    SEXO_CHOICES = [
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('O', 'Outro')
+    ]
+        
+    nome = models.CharField(max_length=200)
+    cpf = models.CharField(max_length=11, unique=True)
+    idade = models.IntegerField(null=True, blank=True)
+    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES)
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+
+
+
