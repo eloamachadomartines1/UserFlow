b7dcb352d918a8c212c8d0cb0d862de01eefba2a
Eloá Machado Martines <eloamachadomartines1@gmail.com>
2026-09-23 15:41:03 -0400

    Update UsuarioAdmin to include photo display in the admin list and adjust template for photo column

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,17 +1,6 @@
 from django.contrib import admin
-from django.utils.html import format_html
 from usuarios.models import Usuario
-
-# Register your models here.
 
 @admin.register(Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ['nome', 'cpf', 'idade', 'sexo']
-
-
-def mostrar_foto(self, obj):
-         if obj.foto:
-             return format_html('<img src="{}" width="50" style="border-radius:5px;" />', obj.foto.url)
-         return "Sem foto"
-
-mostrar_foto.short_description = 'Foto'
